fcf25c62b69622987133ce5a6d8ebd1cd9c78f46
xywai1993 <www163ep@163.com>
2015-04-30 18:33:19 +0800

    Update Fan.418.js

--- Fan.418.js
+++ Fan.418.js
@@ -1,38 +1,9 @@
 /**
- * author：fan
- * Update time：2014年6月21日15:36:35
- */
-
+ * 2015年4月30日11:02:44
+*2014年5月5日10:01:12
+*
+*/
 (function(win){
-
-
-//************* 设置在同级元素事件执行后的状态**********************
-// 同级元素数组， 样式名 ，事件(可选)
-    function setState(arr,css,Event){
-        var eve = Event || 'click' ;
-        for (var i = arr.length - 1; i >= 0; i--) {
-            addEvent(arr[i],eve,function(){
-                for (var i = arr.length - 1; i >= 0; i--) {
-                    //arr[i].className = ''
-                    F(arr[i]).removeClass(css)
-                };
-                //this.className = css
-                F(this).addClass(css)
-            })
-        };
-    }
-
-//****************仿jquery toggle函数****************************
-    function FYPtoggle(obj,fn1,fn2){
-        var index=0   ;
-        addEvent(obj,'click',function(){
-            index++
-            if(index%2 == 0){fn2.call(obj)}
-            else{fn1.call(obj)}
-        })
-
-    }
-
 
 
 ///******************************************
@@ -42,7 +13,7 @@
      return new Fan(vArg,parent)
      } */
 
-    win.F = fan ;
+    win.F = fan;
 
     // 对象
     function Fan(eles){
@@ -56,17 +27,22 @@
 //元素获取
     function fan(vArg,parent){
         var elements= [] ,
-            parent = document.getElementById(parent)||parent;
+            parent = document.getElementById(parent)||parent ;
         switch ( typeof vArg){
             case 'function':
                // addEvent(win,'load',vArg) ;
-               win.addEventListener('load',vArg)
+               win.addEventListener('load',vArg);
                 break;
             case 'string':
                 elements = (parent||document).querySelectorAll(vArg) ;
                 break;
             case 'object':
-                elements.push(vArg)
+			    if(vArg.length){
+					elements = vArg
+				}else{
+				    elements.push(vArg)
+				}
+                
         }
         return new Fan(elements)
 
@@ -83,22 +59,22 @@
     };
 
     Fan.prototype.mapOne = function(callback){
-        var m = this.map(callback) ;
+        var m = this.map(callback);
         return m.length>1? m:m[0]
     };
 
     Fan.prototype.each = function(callback){
 
-        this.map(callback) ;
-        return this ;
+        this.map(callback);
+        return this
     };
 
-   Fan.prototype.dom = function(){
+     Fan.prototype.dom = function(){
        var result = this.map(function(ele){
-           return ele
-       })
+           return ele ;
+       });
        return result.length>1?result:result[0]
-   }
+    };
 
 
 //***********DOM操作***************
@@ -110,7 +86,7 @@
      */
     Fan.prototype.on = function(eve,fn){
         return this.each(function(ele){
-            ele.addEventListener(eve,fn,false)
+            ele.addEventListener(eve,fn,false);
         })
 
     };
@@ -140,13 +116,25 @@
 
     };
 
-    Fan.prototype.show=function(){
+    Fan.prototype.show=function(display){
         return this.each(function(ele){
-            ele.style.display=''
+            ele.style.display= display || 'block' ;
         })
 
     };
-
+    
+	Fan.prototype.toggleDisplay=function(display){ 	   
+		return this.each(function(ele){
+		   var style = getComputedStyle(ele,false)['display'] 
+		   //console.log(style)
+			if(style === 'none'|| style ===''){
+				ele.style.display = display||'block'
+			}else if(style === 'block' || style === display){
+				ele.style.display = 'none'
+			}
+		})
+	};
+    
     Fan.prototype.text = function(text){
         if(typeof text !== 'undefined'){
             return this.each(function(ele){
@@ -173,7 +161,7 @@
 
     };
 
-
+  
     /**
      *
      * @param {Object||String} [josn][string] 或者 样式名
@@ -190,8 +178,12 @@
                     ele.style[attr] = josn[attr]
                 }
             })
+
         }
+
     };
+
+
 
     Fan.prototype.addClass = function(classname){
         return this.each(function(ele){
@@ -230,7 +222,30 @@
     Fan.prototype.eq = function(index){
         return fan(this[index])
     };
-
+    
+	Fan.prototype.index = function(Farr){
+	   for(var i=0;i<Farr.length;i++){		
+	     if(this[0] === Farr[i]){ 		   
+		   return i
+		 }
+	   }
+	   return -1
+	};
+	
+	
+	Fan.prototype.find = function(select){      
+	  //	return fan(select,this[0])  
+	  var aEle = [] 
+	  for(var i=0;i<this.length;i++){
+	  	var a = this[i].querySelectorAll(select)	;
+		for(var j=0;j<a.length;j++){
+		   aEle = aEle.concat(a[j])
+		}  
+		
+	  }
+	  return fan(aEle)
+	};
+	
     /**
      *
      * @param {Object} json
@@ -247,6 +262,41 @@
             fan(ele).css(json) ;
        })
    };
+   
 
     // return F
-})(window)
+})(window) 
+
+
+//************* 设置在同级元素事件执行后的状态**********************
+// 同级元素数组， 样式名 ，事件(可选)
+ function setState(arr,css,Event){ 
+    var oArr = obj(arr)
+	var eve = Event || 'click'  	;
+    oArr.on(eve,function(){
+		oArr.removeClass(css) ;
+		F(this).addClass(css)
+	})
+	
+  }
+
+//************tab 切换********************
+function Ftab(Parr,Carr,css){
+		 Carr.hide().eq(0).show() ;	
+	     Parr.on('click',function(){
+	     var index = F(this).index(Parr) ;
+		 Parr.removeClass(css).eq(index).addClass(css);
+		 Carr.hide().eq(index).show() 	   
+	     })
+	  
+	  }
+	  
+
+function obj(obj){
+  if(typeof obj === 'object'){
+    
+    return obj.nodeType ? F(obj): obj		
+  }else if(typeof obj === 'string'){
+    return F(obj)
+  }
+}	  
